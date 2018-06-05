c528f62266a3cbaabc26cad7c81d4fc3b9cfbb57
Sonja <eena.jokela@gmail.com>
2018-06-05 13:33:10 +0300

    kiintopiste/point osuudet lisätty

---
+++ api/controllers/pointController.js
@@ -0,0 +1,58 @@
+'use strict';
+
+
+var mongoose = require('mongoose'),
+  Point = mongoose.model('Point');
+
+exports.list_all_points = function(req, res) {
+  Point.find({}, function(err, point) {
+    if (err)
+      res.send(err);
+    res.json(point);
+  });
+};
+
+
+exports.create_a_points = function(req, res) {
+  var new_points = new Point(req.body);
+  new_points.save(function(err, point) {
+    if (err)
+      res.send(err);
+    res.json(point);
+  });
+};
+
+
+exports.read_a_points = function(req, res) {
+  Point.findById(req.params.pointId, function(err, point) {
+    if (err)
+      res.send(err);
+    res.json(point);
+  });
+};
+
+
+exports.update_a_points = function(req, res) {
+  Point.findOneAndUpdate({_id: req.params.pointsId}, req.body, {new: true}, function(err, point) {
+    if (err)
+      res.send(err);
+    res.json(point);
+  });
+};
+
+
+exports.delete_a_points = function(req, res) {
+  Point.remove({_id: req.params.pointsId}, function(err, point) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Point successfully deleted' });
+  });
+}; 
+
+exports.find_point_by_ID = function(req, res){
+	Points.find({points:req.params.pointounerId}, function(err, points){
+	if (err)
+      res.send(err);
+    res.json(points);
+  });
+}; 
---
+++ api/models/pointModel.js
@@ -0,0 +1,28 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PointSchema = new Schema ({ //Boxit ja terminaalit
+	
+	pointName:{
+		type: String,
+		required:'Name of the point'
+	},
+	
+	pointAddress:{
+		type: String,
+		required:'Address of the point'
+	},
+
+	pointOuner:{
+		type: String,
+		required:'Company name'
+	}
+	
+	pointounerId:{
+		type: String,
+	}
+
+});
+
+module.exports = mongoose.model('Points', PointSchema);
