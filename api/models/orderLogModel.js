64cd3774e068431e0c33a321756ecd2fa474f9d1
Risto Hietala <risto.hietala@centria.fi>
2019-04-16 11:56:26 +0300

    fix for orderlog

--- api/models/orderLogModel.js
+++ api/models/orderLogModel.js
@@ -15,7 +15,8 @@ var orderLogSchema = new Schema ({
   },
   code:{ //what happened
     type: String,
-    enum: ['customer_created', 'operator_created', 'operator_delivery', 'driver_accept', 'driver_cancel', 'driver_pickup','driver_delivery','operator_cancel', 'operator_archive_1', 'operator_archive_2'],
+    enum: ['customer_created', 'operator_created', 'operator_delivery', 'driver_accept', 'driver_cancel', 'driver_pickup','driver_delivery',
+    'operator_cancel', 'operator_archive_1', 'operator_archive_2','box_accepted','box_cancelled','delivery_ready','delivery_not_ready'],
 		default: ['customer_created']
   },
   orderID:{ // order mongo _id
